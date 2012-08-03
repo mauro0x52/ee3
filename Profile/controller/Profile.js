cc675a17023f317c869bae17e4db6597d470a691
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2012-08-03 11:46:29 -0300

    Revisão da controller de profile do servico de profile

--- Profile/controller/Profile.js
+++ Profile/controller/Profile.js
@@ -4,12 +4,12 @@
  *
  * @description : Módulo que implementa as funcionalidades de profiles de usuários
  */
- 
+
 module.exports = function (app) {
     var Model = require('./../model/Model.js'),
         auth = require('./../Utils.js').auth,
         Profile  = Model.Profile;
-        
+
     /** GET /profile/:slug
      *
      * @autor : Lucas Kalado
@@ -26,21 +26,20 @@ module.exports = function (app) {
     app.get('/profile/:slug', function (request,response) {
         response.contentType('json');
         
-        //Verifica se existe o parametro Slug
-        if (request.params.slug) {
-            //Localiza o Profile
-            Profile.findOne({"slugs.name" : request.params.slug}, function (error, profile) {
-                if (error) {
-                    response.send({error: error});
+        //Localiza o Profile
+        Profile.findOne({"slugs.name" : request.params.slug}, function (error, profile) {
+            if (error) {
+                response.send({error: error});
+            } else {
+                if (profile === null) {
+                    response.send({error : 'profile not found'});
                 } else {
-                    response.send({Profile: profile});
+                    response.send({profile: profile});
                 }
-            });
-        } else {
-            response.send({error: "É necessário o envio de uma slug"});
-        }
+            }
+        });
     });
-    
+
     /** POST /profile
      *
      * @autor : Lucas Kalado
@@ -55,140 +54,25 @@ module.exports = function (app) {
      * @response : {this}
      */
     app.post('/profile', function (request,response) {
-        var profile,
-            jobs,
-            slugs,
-            validateSlugs,
-            thumbnails,
-            phones,
-            contacts,
-            links,
-            parsJobs = [],
-            parsSlugs = [],
-            parsThumbnails = [],
-            parsPhones = [],
-            parsContacts = [],
-            parsLinks = [];
-        
+        var profile;
+
         response.contentType('json');
-        
+
         //Verifica se o usuário logado é válido
         auth(request.param('login'), request.param('token'), function (valid) {
             if (valid) {
-                //Verifica se existe o parametro Jobs e trata os dados para adicionar no Model
-                if (request.param('jobs', null)) {
-                    jobs = request.param('jobs', null);
-                    //Percorre entre os Jobs enviados
-                    for(var job in jobs){
-                        if (jobs.hasOwnProperty(job)) {
-                            var temp = {};
-                            //Percorre entre cada campo do job enviado
-                            for(var jobField in jobs[job]){
-                                if (jobs[job].hasOwnProperty(jobField)) {
-                                    temp[jobField] = jobs[job][jobField];
-                                }
-                            }
-                            parsJobs.push(temp);
-                        }
-                    }
-                }
-                //Verifica se existe o parametro Slugs e trata os dados para adicionar no Model
-                if (request.param('slugs', null)) {
-                    slugs = request.param('slugs', null);
-                    //Percorre entre os Slugs enviados
-                    for(var slug in slugs){
-                        if (slugs.hasOwnProperty(slug)) {
-                            var temp = {};
-                            //Percorre entre cada campo do Slug enviado
-                            for(var slugField in slugs[slug]){
-                                if (slugs[slug].hasOwnProperty(slugField)) {
-                                    temp[slugField] = slugs[slug][slugField];
-                                }
-                            }
-                            parsSlugs.push(temp);
-                        }
-                    }
-                }
-                //Verifica se existe o parametro thumbnails e trata os dados para adicionar no Model
-                if (request.param('thumbnails', null)) {
-                    thumbnails = request.param('thumbnails', null);
-                    //Percorre entre os Thumbnails enviados
-                    for(var thumbnail in thumbnails){
-                        if (thumbnails.hasOwnProperty(thumbnail)) {
-                            var temp = {};
-                            //Percorre entre cada campo do Thumbnail enviado
-                            for(var thumbnailField in thumbnails[thumbnail]){
-                                if (thumbnails[thumbnail].hasOwnProperty(thumbnailField)) {
-                                    temp[thumbnailField] = thumbnails[thumbnail][thumbnailField];
-                                }
-                            }
-                            parsThumbnails.push(temp);
-                        }
-                    }
-                }
-                //Verifica se existe o parametro phones e trata os dados para adicionar no Model
-                if (request.param('phones', null)) {
-                    phones = request.param('phones', null);
-                    //Percorre entre os Phones enviados
-                    for(var phone in phones){
-                        if (phones.hasOwnProperty(phone)) {
-                            var temp = {};
-                            //Percorre entre cada campo do Phone enviado
-                            for(var phoneField in phones[phone]){
-                                if (phones[phone].hasOwnProperty(phoneField)) {
-                                    temp[phoneField] = phones[phone][phoneField];
-                                }
-                            }
-                            parsPhones.push(temp);
-                        }
-                    }
-                }
-                //Verifica se existe o parametro contacts e trata os dados para adicionar no Model
-                if (request.param('contacts', null)) {
-                    contacts = request.param('contacts', null);
-                    //Percorre entre os Contacts enviados
-                    for(var contact in contacts){
-                        if (contacts.hasOwnProperty(contact)) {
-                            var temp = {};
-                            //Percorre entre cada campo do Contact enviado
-                            for(var contactField in contacts[contact]){
-                                if (contacts[contact].hasOwnProperty(contactField)) {
-                                    temp[contactField] = contacts[contact][contactField];
-                                }
-                            }
-                            parsContacts.push(temp);
-                        }
-                    }
-                }
-                //Verifica se existe o parametro links e trata os dados para adicionar no Model
-                if (request.param('links', null)) {
-                    links = request.param('links', null);
-                    //Percorre entre os Links enviados
-                    for(var link in links){
-                        if (links.hasOwnProperty(link)) {
-                            var temp = {};
-                            //Percorre entre cada campo do Link enviado
-                            for(var linkField in links[link]){
-                                if (links[link].hasOwnProperty(linkField)) {
-                                    temp[linkField] = links[link][linkField];
-                                }
-                            }
-                            parsLinks.push(temp);
-                        }
-                    }
-                }
                 //Cria o Objeto Profile para adicionar no Model
                 profile = new Profile({
                     users       : request.param('login', null),
-                    jobs        : parsJobs,
-                    slugs       : parsSlugs,
+                    jobs        : request.param('jobs', null),
+                    slugs       : request.param('slugs', null),
                     name        : request.param('name', null),
                     surname     : request.param('surname', null),
-                    thumbnail   : parsThumbnails,
+                    thumbnail   : request.param('thumbnails', null),
                     about       : request.param('about', null),
-                    phones      : parsPhones,
-                    contacts    : parsContacts,
-                    links       : parsLinks,
+                    phones      : request.param('phones', null),
+                    contacts    : request.param('contacts', null),
+                    links       : request.param('links', null),
                     dateCreated : new Date(),
                     dateUpdated : new Date()
                 });
@@ -197,15 +81,15 @@ module.exports = function (app) {
                     if (error) {
                         response.send({error : error});
                     } else {
-                        response.send({profile : profile});
+                        response.send({error : ''});
                     }
                 });
             } else {
-                request.send({error : error});
+                request.send({error : 'invalid token'});
             }
         })
     });
-    
+
     /** PUT /profile/:slug
      *
      * @autor : Lucas Kalado
@@ -220,43 +104,22 @@ module.exports = function (app) {
      * @response : {this}
      */
     app.put('/profile/:slug', function (request,response) {
-        var profile,
-            slugs,
-            parsSlugs = [];
-        
         response.contentType('json');
         
         //Verifica se o usuário logado é válido
         auth(request.param('login'), request.param('token'), function (valid) {
-            //Verifica se existe o parametro Slug
-            if (request.params.slug) {
+            if (valid) {
                 //Localiza o Profile
-                Profile.findOne({"slugs.name":request.params.slug}, function (error, profile) {
+                Profile.findOne({"slugs.name" : request.params.slug}, function (error, profile) {
                     if (error) {
                         response.send({error: error});
                     } else {
                         //Verifica se o Profile foi encontrado
-                        if (profile) {
-                            //Verifica se existe o parametro Slugs e trata os dados para adicionar no Model
-                            if (request.param('slugs', null)) {
-                                slugs = request.param('slugs', null);
-                                //Percorre entre os Slugs enviados
-                                for(var slug in slugs){
-                                    if (slugs.hasOwnProperty(slug)) {
-                                        var temp = {};
-                                        //Percorre entre cada campo do Slug enviado
-                                        for(var slugField in slugs[slug]){
-                                            if (slugs[slug].hasOwnProperty(slugField)) {
-                                                temp[slugField] = slugs[slug][slugField];
-                                            }
-                                        }
-                                        //adiciona a variável parsSlugs os campos encontrados
-                                        parsSlugs.push(temp);
-                                    }
-                                }
-                            }
-                            //Alterando o Profile
+                        if (profile === null) {
+                            response.send({error : "profile not found"});
+                        } else {
                             this.name = request.param('name', null);
+                            this.slugs = request.param('slugs', null);
                             this.surname = request.param('surname', null);
                             this.about = request.param('about', null);
                             this.dateUpdated = new Date();
@@ -264,20 +127,18 @@ module.exports = function (app) {
                                 if (error) {
                                     response.send({error : error});
                                 } else {
-                                    response.send({Profile : profile});
+                                    response.send({error : ''});
                                 }
                             });
-                        } else {
-                            response.send({error: "Profile não encontrado."});
                         }
                     }
                 });
             } else {
-                response.send({error: "É necessário o envio de uma slug"});
+                request.send({error : 'invalid token'});
             }
         });
     });
-    
+
     /** DEL /profile/:slug
      *
      * @autor : Lucas Kalado
@@ -304,7 +165,7 @@ module.exports = function (app) {
                     } else {
                         //verifica se o profile foi encontrado
                         if (profile === null) {
-                            response.send({error : 'Profile não encontrado'});
+                            response.send({error : 'profile not found'});
                         } else {
                             //remove o profile
                             profile.remove(function (error) {
@@ -322,5 +183,4 @@ module.exports = function (app) {
             }
         });
     });
-    
 };
