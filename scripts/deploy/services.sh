401d468f1a533c4c8d4febaaadf9754fc90a818f
Mauro Ribeiro <mauro0x52@gmail.com>
2012-08-08 08:54:22 -0300

    Atualização da lista de serviços do continous deployment

--- scripts/deploy/services.sh
+++ scripts/deploy/services.sh
@@ -4,5 +4,10 @@
 # Lista de Servicos
 # ------------------------------------------------------------------------------
 
-SERVICES_NAMES[0]="Auth"
-SERVICES_NAMES[1]="Apps"
+SERVICES_NAMES[0]="Apps"
+SERVICES_NAMES[1]="Auth"
+SERVICES_NAMES[2]="Companies"
+SERVICES_NAMES[3]="Files"
+SERVICES_NAMES[4]="Location"
+SERVICES_NAMES[5]="Profile"
+SERVICES_NAMES[6]="Talk"
