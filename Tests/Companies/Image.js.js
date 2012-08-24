2fb00f64a12d5d4fa2d2b85552aa119e90d774ec
Mauro Ribeiro <mauro0x52@gmail.com>
2012-08-24 11:07:22 -0300

    Iniciando testes de imagens de produtos

---
+++ Tests/Companies/Image.js.js
@@ -0,0 +1,58 @@
+/** Testes  Companies.Image
+ *
+ * @autor : Mauro Ribeiro
+ * @since : 2012-08
+ *
+ * @description : Kit de testes do controller Image do serviço Companies
+ */
+
+var should = require("should"),
+    api = require("../Utils.js").api,
+    rand = require("../Utils.js").rand,
+    companyImageUrl, productImageUrl, random,
+    user, company, product;
+
+describe('POST /company/:company_slug/product/:product_slug/image', function() {
+    it('token inválido');
+    it('empresa não existe');
+    it('empresa sem produtos');
+    it('produto não existe');
+    it('empresa de outro usuário');
+    it('não escolhe imagem');
+    it('envia imagem');
+});
+
+describe('GET /company/:company_slug/product/:product_slug/images', function() {
+    it('empresa não existe');
+    it('produto não existe');
+    it('empresa sem produtos');
+    it('produto sem imagens');
+    it('lista de imagens');
+});
+
+describe('GET /company/:company_slug/product/:product_slug/image/:id', function() {
+    it('empresa não existe');
+    it('produto não existe');
+    it('empresa sem produtos');
+    it('produto sem imagens');
+    it('imagem não existe');
+    it('dados da imagens');
+});
+
+describe('PUT /company/:company_slug/product/:product_slug/image/:id', function() {
+    it('empresa não existe');
+    it('produto não existe');
+    it('empresa sem produtos');
+    it('produto sem imagens');
+    it('imagem não existe');
+    it('edita dados da imagem');
+});
+
+describe('DEL /company/:company_slug/product/:product_slug/image/:id', function() {
+    it('empresa não existe');
+    it('produto não existe');
+    it('empresa sem produtos');
+    it('produto sem imagens');
+    it('imagem não existe');
+    it('apaga imagem');
+});
