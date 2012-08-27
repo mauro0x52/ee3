cbfd9fa2c48d6875483445f30ea10a676aeaf56a
Rafael Almeida Erthal Hermano <rafaerthal@gmail.com>
2012-08-27 11:18:57 -0300

    teste do Contato no Profile

--- Profiles/model/Contact.js
+++ Profiles/model/Contact.js
@@ -11,8 +11,8 @@ var mongoose = require('mongoose'),
     contactSchema;
 
 contactSchema = new schema({
-    type    : {type : String, required : true, enum : ['twitter', 'msn', 'email', 'skype', 'gtalk']},
-    address : {type : String, trim : true}
+    type    : {type : String, required : true, enum : ['Twitter', 'Msn', 'Email', 'Skype', 'Gtalk']},
+    address : {type : String, required : true, trim : true}
 });
 
 /*  Exportando o pacote  */
