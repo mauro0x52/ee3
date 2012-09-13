0d5ef33e88ecda4d36b28d6c878fbdc1aff8867c
Mauro Ribeiro <mauro0x52@gmail.com>
2012-09-13 17:12:40 -0300

    Reescrevendo todos os erros PermissionDenied

--- Apps/controller/Plugin.js
+++ Apps/controller/Plugin.js
@@ -47,7 +47,7 @@ module.exports = function (app) {
                         } else {
                             //verifica se o usuário é o criador do app
                             if (user._id !== app.creator) {
-                                response.send({error : 'permission denied', name : 'PermisionDenied'});
+                                response.send({error : {message : 'permission denied', name : 'PermissionDenied'}});
                             } else {
                                 //busca a versão
                                 app.findVersion(request.params.number, function (error, version) {
